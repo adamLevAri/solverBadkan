1b47603bf6e89a2b3da494d87509acc326709901
adamLevAri <37263805+adamLevAri@users.noreply.github.com>
2020-03-07 23:05:40 +0200

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -37,22 +37,3 @@ TEST_CASE("Test case 2") {
     CHECK(solve(2*y-4 == 10) == 7);
     CHECK(solve((y^2) == 16) == 4);
 }
-
-#include "badkan.h"
-
-int main(){
-
-    VAR x;
-
-    badkan::TestCase testcase;
-    testcase.setname("My tests")
-        .CHECK_EQUAL(solve((2*x^2) - 4*x + 3 == 9), 3)
-        .CHECK_EQUAL(solve((x^2) + 6*x == -9), -3)
-        .CHECK_EQUAL(solve(2*x-4 == 10), 7)
-        .CHECK_EQUAL(solve((x^2) == 16),4)
-        .print();
-
-    
-    return 0;
-}
-
