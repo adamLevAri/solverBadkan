a44f222765056c536876d54593e5f7db88adec84
adamLevAri <37263805+adamLevAri@users.noreply.github.com>
2020-03-07 22:57:41 +0200

    Update and rename solver.h to solver.hpp

--- solver.hpp
+++ solver.hpp
@@ -20,13 +20,6 @@ using namespace std;
 POLI operator ^ (LINE A, int e){
     POLI R(A.getA(), 0, A.getB(), e);
     R.setX(A.getX());
-    /*
-    POLI R;
-    R.a = A.a;
-    R.c = A.b;
-    R.e = e;
-    R.x = A.x;
-    */
      return R;
 }
 
@@ -34,25 +27,12 @@ LINE operator + (LINE A, LINE B){
     LINE R(A.getA()+B.getA(), A.getB()+B.getB());
     if(A.getX()) R.setX(A.getX());
     else R.setX(B.getX());
-    /*
-    R.b = A.b + B.b;
-    R.a = A.a + B.a;
-    if(A.x) R.x = A.x;
-    else R.x = B.x;
-    */
      return R;
 }
 
 POLI operator + (POLI A, LINE B){
     POLI R(A.getA(), A.getB()+B.getA(), A.getC()+B.getB(), A.getE());
     R.setX(A.getX());
-    /*
-    R.a = A.a;
-    R.b = A.b + B.a;
-    R.c = A.c + B.b;
-    R.e = A.e;
-    R.x = A.x;
-    */
     return R;
 }
 
@@ -60,25 +40,12 @@ LINE operator - (LINE A, LINE B){//same as +
     LINE R(A.getA()-B.getA(), A.getB()-B.getB());
     if (A.getX()) R.setX(A.getX());
     else R.setX(B.getX());
-    /*
-    R.b = A.b - B.b;
-    R.a = A.a - B.a;
-    if(A.x) R.x = A.x;
-    else R.x = B.x;
-    */
     return R;
 }
 
 POLI operator - (POLI A, LINE B){
     POLI R(A.getA(), A.getB()-B.getA(), A.getC()-B.getB(), A.getE());
     R.setX(A.getX());
-    /*
-    R.a = A.a;
-    R.b = A.b - B.a;
-    R.c = A.c - B.b;
-    R.e = A.e;
-    R.x = A.x;
-    */
     return R;
 }
 
@@ -86,23 +53,12 @@ LINE operator * (LINE A, LINE B){
     LINE R(A.getA()*B.getB()+B.getA()*A.getB(), A.getB()*B.getB());
     if (A.getX()) R.setX(A.getX());
     else R.setX(B.getX());
-    /*
-    R.a = A.a * B.b + B.a * A.b;
-    R.b = A.b * B.b;
-    if(A.x) R.x = A.x;
-    else R.x = B.x;
-    */
     return R;
 }
 
 LINE operator / (LINE A, LINE B){
     LINE R(A.getA()/B.getB(), A.getB()/B.getB());
     R.setX(A.getX());
-    /*
-    R.b = A.b / B.b;
-    R.a = A.a / B.b;
-    R.x = A.x;
-    */
     return R;
 }
 
