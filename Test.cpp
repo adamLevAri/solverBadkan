217001c7b8b88a00fd2e7c2b3cc3f24bafbb8237
adamLevAri <37263805+adamLevAri@users.noreply.github.com>
2020-03-08 03:48:59 +0200

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -13,17 +13,17 @@ TEST_CASE("Test case 1") {
    solver::Variable x;
    
    
-   CHECK((solve((2*x^2) - 4*x + 3 == 9)) == (3));
-    CHECK(solve((2*x^2) - 4*x + 3 == 9) == (3));
-    CHECK(solve((x^2) + 6*x == -9) == (-3));
-    CHECK(solve(2*x-4 == 10) == (7));
-    CHECK(solve((x^2) == 16) == (4));
+   CHECK((solve((2*x^2) - 4*x + 3 == 9)) == (3.0));
+    CHECK(solve((2*x^2) - 4*x + 3 == 9) == (3.0));
+    CHECK(solve((x^2) + 6*x == -9) == (-3.0));
+    CHECK(solve(2*x-4 == 10) == (7.0));
+    CHECK(solve((x^2) == 16) == (4.0));
     
-   CHECK((solve((2*x^2) - 4*x + 3 == 9)) == (3));
-    CHECK(solve((2*x^2) - 4*x + 3 == 9) == (3));
-    CHECK(solve((x^2) + 6*x == -9) == (-3));
-    CHECK(solve(2*x-4 == 10) == (7));
-    CHECK(solve((x^2) == 16) == (4));
+   CHECK((solve((2*x^2) - 4*x + 3 == 9)) == (3.0));
+    CHECK(solve((2*x^2) - 4*x + 3 == 9) == (3.0));
+    CHECK(solve((x^2) + 6*x == -9) == (-3.0));
+    CHECK(solve(2*x-4 == 10) == (7.0));
+    CHECK(solve((x^2) == 16) == (4.0));
     
 }
 
@@ -31,15 +31,15 @@ TEST_CASE("Test case 2") {
     solver::Variable y;
     
    
-   CHECK((solve((2*y^2) - 4*y + 3 == 9)) == (3));
-    CHECK(solve((2*y^2) - 4*y + 3 == 9) == (3));
-    CHECK(solve((y^2) + 6*y == -9) == (-3));
-    CHECK(solve(2*y-4 == 10) == (7));
-    CHECK(solve((y^2) == 16) == (4));
+   CHECK((solve((2*y^2) - 4*y + 3 == 9)) == (3.0));
+    CHECK(solve((2*y^2) - 4*y + 3 == 9) == (3.0));
+    CHECK(solve((y^2) + 6*y == -9) == (-3.0));
+    CHECK(solve(2*y-4 == 10) == (7.0));
+    CHECK(solve((y^2) == 16) == (4.0));
 
-    CHECK((solve((2*y^2) - 4*y + 3 == 9)) == (3));
-    CHECK(solve((2*y^2) - 4*y + 3 == 9) == (3));
-    CHECK(solve((y^2) + 6*y == -9) == (-3));
-    CHECK(solve(2*y-4 == 10) == (7));
-    CHECK(solve((y^2) == 16) == (4));
+    CHECK((solve((2*y^2) - 4*y + 3 == 9)) == (3.0));
+    CHECK(solve((2*y^2) - 4*y + 3 == 9) == (3.0));
+    CHECK(solve((y^2) + 6*y == -9) == (-3.0));
+    CHECK(solve(2*y-4 == 10) == (7.0));
+    CHECK(solve((y^2) == 16) == (4.0));
 }
